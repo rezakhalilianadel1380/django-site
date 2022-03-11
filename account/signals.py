e2b0c08c7b49dcbcb93f12dbd09211e7535cb8b0
rezadeveloper <38360147+rezakhalilianadel1380@users.noreply.github.com>
2022-03-11 19:37:08 +0330

    test

--- account/signals.py
+++ account/signals.py
@@ -2,9 +2,9 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import User_detail
- 
- 
+
+
 @receiver(post_save, sender=User)
-def create_profile(sender,instance,created, **kwargs):
+def create_profile(sender, instance, created, **kwargs):
     if created:
         User_detail.objects.create(user=instance)
