ff9ccb3ff61643e5bf81960b2d0b0daf93e2f5c1
rezadeveloper <38360147+rezakhalilianadel1380@users.noreply.github.com>
2022-03-12 12:54:16 +0330

    remove comment of singals

--- account/signals.py
+++ account/signals.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import User_detail
 
-#this func recive signals and create user_detail instance 
+ 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
